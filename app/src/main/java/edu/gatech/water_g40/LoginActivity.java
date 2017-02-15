2d95f3b2bc0d3b036990c28257e3ec07a9ba45ae
jhpinder <jhpinder@gmail.com>
2017-02-15 15:52:30 -0500

    edited login screen

--- app/src/main/java/edu/gatech/water_g40/LoginActivity.java
+++ app/src/main/java/edu/gatech/water_g40/LoginActivity.java
@@ -191,13 +191,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.equals("user");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.equals("pass");
     }
 
     /**
