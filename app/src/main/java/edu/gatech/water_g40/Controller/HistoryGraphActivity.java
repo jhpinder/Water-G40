8f9d3c6078db28bd917078f28ae859533d4f18ec
jhpinder <jhpinder@gmail.com>
2017-04-04 20:47:46 -0400

    added history activity

---
+++ app/src/main/java/edu/gatech/water_g40/Controller/HistoryGraphActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.water_g40.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.water_g40.R;
+
+public class HistoryGraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_graph);
+    }
+}
