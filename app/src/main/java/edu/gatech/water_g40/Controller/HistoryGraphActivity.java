971d87553095f0a33d00f19f8a8d1a228d02a65f
jhpinder <jhpinder@gmail.com>
2017-04-04 21:04:28 -0400

    added history button to main menu

--- app/src/main/java/edu/gatech/water_g40/Controller/HistoryGraphActivity.java
+++ app/src/main/java/edu/gatech/water_g40/Controller/HistoryGraphActivity.java
@@ -3,6 +3,10 @@ package edu.gatech.water_g40.Controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import edu.gatech.water_g40.R;
 
 public class HistoryGraphActivity extends AppCompatActivity {
@@ -11,5 +15,14 @@ public class HistoryGraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_graph);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 }
